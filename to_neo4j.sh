8206fb0fe96414e58e905412200d5a3a0d4bdab3
qthurier <quentin.thurier@gmail.com>
2017-05-17 10:41:07 +1200

    add csv files for neo4j

--- to_neo4j.sh
+++ to_neo4j.sh
@@ -1,19 +1,21 @@
 
 FOLDER=out
 
-rm -r /usr/local/Cellar/neo4j/3.1.1/libexec/data/databases/pdh_data_review.db
+rm -r /usr/local/Cellar/neo4j/3.1.1/libexec/data/databases/graph.db
 
 neo4j-import --stacktrace \
              --multiline-fields=true \
-             --into /usr/local/Cellar/neo4j/3.1.1/libexec/data/databases/pdh_data_review.db \
+             --into /usr/local/Cellar/neo4j/3.1.1/libexec/data/databases/graph.db \
              --id-type string \
              --nodes:Dataset $FOLDER/dataset.csv \
              --nodes:Provider $FOLDER/provider.csv  \
              --nodes:Tag $FOLDER/tag.csv \
+             --nodes:Level1 $FOLDER/level1.csv \
              --nodes:Category $FOLDER/category.csv \
              --relationships:provides $FOLDER/provider_dataset.csv \
              --relationships:contains_information_about $FOLDER/dataset_tag.csv \
              --relationships:fall_into $FOLDER/dataset_category.csv \
-             --relationships:provides_dataset_falling_into $FOLDER/provider_category.csv
+             --relationships:provides_dataset_falling_into $FOLDER/provider_category.csv \
+             --relationships:is_detailed_in $FOLDER/dataset_level1.csv
 
 
